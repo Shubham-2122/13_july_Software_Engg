4e9975f65700415ab19b0f86044e3fd012c465b5
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-07-20 14:46:56 +0530

    c

---
+++ C/05_loops/04_netste.c
@@ -0,0 +1,31 @@
+/*
+	for(){
+		for(){
+		}
+	}
+	
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	
+	int r,c;
+	
+	for(r=1;r<=5;r++){
+		for(c=1;c<=5;c++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+	getch();
+}
---
+++ C/05_loops/05_netsted_ex.c
@@ -0,0 +1,32 @@
+/*
+	for(){
+		for(){
+		}
+	}
+	
+	* 
+	* * 
+	* * * 
+	* * * *
+	* * * * *
+	
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	
+	int r,c;
+
+	for(r=1;r<=5;r++){
+		for(c=1;c<=r;c++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	
+	
+	getch();
+}
